ddb269607eed51f20f9e66427be277533808836f
Daniel <danielu96@wp.pl>
2025-06-18 17:13:15 +0200

    add LinksDropdown

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Logo from './Logo';
+import DarkMode from './DarkMode';
+import LinksDropdown from './LinksDropdown';
 function Navbar() {
     return (
         <div className='navcontainer flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8 shadow-md '>
             <Logo />
-            <div className='grid grid-cols-2'></div>
-            <div >   menu      </div>
+            <div className='grid grid-cols-2'>
+                <DarkMode />
+                <div className='grid grid-cols-2'>menu
+                    <LinksDropdown />
+                </div>
+            </div>
         </div>
     );
 }
