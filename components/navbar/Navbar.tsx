11f9472925d666d767f8986326643ae33993189e
Daniel <danielu96@wp.pl>
2025-06-23 16:47:47 +0200

    add navcontainer class

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,15 +4,17 @@ import DarkMode from './DarkMode';
 import LinksDropdown from './LinksDropdown';
 function Navbar() {
     return (
-        <div className='navcontainer flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8 shadow-md '>
-            <Logo />
-            <div className='grid grid-cols-2'>
-                <DarkMode />
-                <div className='grid grid-cols-2'>menu
-                    <LinksDropdown />
+        <>
+            <div className='w-full shadow-md '>
+                <div className='navcontainer flex sm-flex-col sm:flex-row justify-between  sm:items-center flex-wrap gap-4 py-8 '>
+                    <Logo />
+                    <div className='grid grid-cols-2  justify-items-start '>
+                        <DarkMode />
+                        <LinksDropdown />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 export default Navbar;
