0b63cb1513c57cc22615d92a86298dd7b8d2ea56
Daniel <danielu96@wp.pl>
2025-06-23 16:49:15 +0200

    add bg-blue for button

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -5,7 +5,7 @@ import { Button } from '../ui/button';
 import { Gi3dHammer } from "react-icons/gi";
 function Logo() {
     return (
-        <Button className='min-w-max p-3 gap-2 ' size='icon' asChild>
+        <Button className='min-w-max p-3 gap-2 bg-blue-500 ' size='icon' asChild>
             <Link className='flex' href='/'>
                 <Gi3dHammer className='size-5' />
                 <span>SlotifyCal</span>
