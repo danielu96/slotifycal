6a47ad5c3f3920472ff030664cb1401b1d79fad8
Daniel <danielu96@wp.pl>
2025-06-26 10:12:49 +0200

    fix the appearance

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import UserCalendar from "@/components/UserCalendar";
 export default function Home() {
   return (
     <>
-      <div className="container min-h-[700px] shadow-none md:shadow-md rounded-lg p-3 mt-5">
+      <div className="container max-w-7xl  min-h-[700px] md:min-h-auto shadow-none md:shadow-md rounded-lg p-3 mt-5">
         <div className="grid grid-cols-1 gap-4">
           <UserCalendar />
         </div>
