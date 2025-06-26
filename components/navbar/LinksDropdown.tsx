6ccdc4fb97c9a67da25b024e9877ae35836023b7
Daniel <danielu96@wp.pl>
2025-06-26 10:14:47 +0200

    add SignedOut at SignedIn

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -9,10 +9,11 @@ import { LuAlignLeft } from 'react-icons/lu';
 import Link from 'next/link';
 import { Button } from '../ui/button';
 // import UserIcon from './UserIcon';
+import UserIcon from './UserIcon';
 // import { links } from '@/utils/links';
 import { links } from '@/utils/links'; // Adjust the import path as necessary
-// import SignOutLink from './SignOutLink';
-// import { SignedOut, SignedIn, SignInButton, SignUpButton } from '@clerk/nextjs';
+import SignOutLink from './SignOutLink';
+import { SignedOut, SignedIn, SignInButton, SignUpButton } from '@clerk/nextjs';
 // import { auth } from '@clerk/nextjs/server';
 function LinksDropdown() {
     //   const { userId } = auth();
@@ -23,39 +24,39 @@ function LinksDropdown() {
                 <Button variant='outline' className='flex gap-4 max-w-[100px]'>
                     {/* <LuAlignLeft className='w-6 h-6' /> */}
                     menu
-                    {/* <UserIcon /> */}
+                    <UserIcon />
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className='w-52' align='start' sideOffset={10}>
-                {/* <SignedOut> */}
-                <DropdownMenuItem>
-                    {/* <SignInButton mode='modal'> */}
-                    <button className='w-full text-left'>Login</button>
-                    {/* </SignInButton> */}
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    {/* <SignUpButton mode='modal'> */}
-                    <button className='w-full text-left'>Register</button>
-                    {/* </SignUpButton> */}
-                </DropdownMenuItem>
-                {/* </SignedOut> */}
-                {/* <SignedIn> */}
-                {links.map((link) => {
-                    // if (link.label === 'admin' && !isAdminUser) return null;
-                    return (
-                        <DropdownMenuItem key={link.href}>
-                            <Link href={link.href} className='capitalize w-full'>
-                                {link.label}
-                            </Link>
-                        </DropdownMenuItem>
-                    );
-                })}
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    {/* <SignOutLink /> */}
-                </DropdownMenuItem>
-                {/* </SignedIn> */}
+                <SignedOut>
+                    <DropdownMenuItem>
+                        <SignInButton mode='modal'>
+                            <button className='w-full text-left'>Login</button>
+                        </SignInButton>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        {/* <SignUpButton mode='modal'> */}
+                        <button className='w-full text-left'>Register</button>
+                        {/* </SignUpButton> */}
+                    </DropdownMenuItem>
+                </SignedOut>
+                <SignedIn>
+                    {links.map((link) => {
+                        // if (link.label === 'admin' && !isAdminUser) return null;
+                        return (
+                            <DropdownMenuItem key={link.href}>
+                                <Link href={link.href} className='capitalize w-full'>
+                                    {link.label}
+                                </Link>
+                            </DropdownMenuItem>
+                        );
+                    })}
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <SignOutLink />
+                    </DropdownMenuItem>
+                </SignedIn>
             </DropdownMenuContent>
         </DropdownMenu>
     );
