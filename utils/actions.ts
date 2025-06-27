14ce4b2e2bccee1af736fdb865feeb940c0a2d2d
Daniel <danielu96@wp.pl>
2025-06-27 14:21:02 +0200

    add updateImageAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,7 +1,10 @@
 'use server';
 import db from './db';
 import { auth, currentUser } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { validateWithZodSchema, imageSchema } from './schemas';
+import { uploadImage } from './supabase';
 
 export const fetchProfileImage = async () => {
     const user = await currentUser();
@@ -43,4 +46,28 @@ const renderError = (error: unknown): { message: string } => {
     return {
         message: error instanceof Error ? error.message : 'An error occurred',
     };
+};
+export const updateProfileImageAction = async (
+    prevState: any,
+    formData: FormData
+) => {
+    const user = await getAuthUser();
+    try {
+        const image = formData.get('image') as File;
+        const validatedFields = validateWithZodSchema(imageSchema, { image });
+        const fullPath = await uploadImage(validatedFields.image);
+
+        await db.profile.update({
+            where: {
+                clerkId: user.id,
+            },
+            data: {
+                profileImage: fullPath,
+            },
+        });
+        revalidatePath('/profile');
+        return { message: 'Profile image updated successfully' };
+    } catch (error) {
+        return renderError(error);
+    }
 };
