a8e7070792446cbf75336a87101c42b4e076d08d
Daniel <danielu96@wp.pl>
2025-08-02 14:54:02 +0200

    add testPage

---
+++ app/test/page.tsx
@@ -0,0 +1,46 @@
+
+import React from "react";
+import { fetchReservations } from "@/utils/actions";
+import DeleteReservation from "@/components/DelRes";
+
+export default async function ReservationsPage() {
+    const reservations = await fetchReservations();
+
+    return (
+        <>
+            <div className="container mx-auto p-4">
+                <h1 className="text-2xl font-bold mb-4">Your Reservations</h1>
+                <p className="mb-4">You have {reservations.length} reservations.</p>
+                {reservations.length ? (
+                    <ul className="space-y-4">
+                        {reservations.map((r) => (
+                            <li
+                                key={r.id}
+                                className="p-4 border rounded-lg flex justify-between"
+                            >
+                                <div>
+                                    <p>Time: {r.time}</p>
+                                    <p className="text-sm text-gray-500">
+                                        Date: {new Date(r.date).toLocaleDateString()}
+                                    </p>
+                                </div>
+                                <DeleteReservation reservationId={r.id} />
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>No reservations found.</p>
+                )}
+            </div>
+
+        </>
+    );
+}
+
+
+
+
+
+
+
+
